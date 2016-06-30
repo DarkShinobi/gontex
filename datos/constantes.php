a4a2b63c075cb1a12f52128eb1b07cf20688fe09
Adrian Guzman <edgar.guzman21@gmail.com>
2016-06-30 13:17:31 -0500

    se agregar archivo con datos de conexion

---
+++ datos/constantes.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_NOMBRE_DATABASE','mohansof_gontex');
+define('DB_USUARIO','mohansof_gtdb');
+define('DB_PUERTO','3307');
+define('DB_CLAVE','abc123.');
